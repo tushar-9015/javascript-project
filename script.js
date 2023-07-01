5006555fa0bc362714a74e1063545dd1c14a78bf
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-07-01 15:36:09 +0530

    commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var crsr = document.querySelector("#cursor")
+document.addEventListener("mousemove", function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+})
+
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    height: "110px",
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1,
+    }
+})
+
+gsap.to("#main",{
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroll: "body",
+        start: "top -25%",
+        end: "top -70%",
+        scrub: 2,
+    }
+})
