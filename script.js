f9b7e995995131d3fdf5beeea2f74a78694b6acf
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-07-03 01:06:54 +0530

    commit

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ document.addEventListener("mousemove", function(dets){
     blur.style.top = dets.y - 250 +"px"
 })
 
+var h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale = 5
+        crsr.style.border = "1px solid #fff"
+        crsr.style.backgroundColor = "transparent"
+    });
+    elem.addEventListener("mouseleave", function () {
+        crsr.style.scale = 1;
+        crsr.style.border = "0px solid #95C11E";
+        crsr.style.backgroundColor = "#95C11E";
+    });
+})
+
 
 gsap.to("#nav", {
     backgroundColor: "#000",
@@ -30,5 +44,68 @@ gsap.to("#main",{
         start: "top -25%",
         end: "top -70%",
         scrub: 2,
+    }
+})
+
+gsap.from("#about img,#about-us",{
+    y:50,
+    opacity:0,
+    duration:1,
+    stagger:0.4,
+    scrollTrigger:{
+        trigger:"#about",
+        scroller:"body",
+        start:"top 70%",
+        end:"top 65%",
+        scrub:2,
+    }
+})
+
+gsap.from(".card",{
+    scale:0.5,
+    opacity:0,
+    duration:1,
+    stagger:0.2,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        start:"top 70%",
+        end:"top 65%",
+        scrub:1,
+    }
+})
+
+gsap.from("#colon1",{
+    x: -70,
+    y: -70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:3,
+    }
+})
+
+gsap.from("#colon2",{
+    x: 70,
+    y: 70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:3,
+    }
+})
+
+gsap.from("#page4 h1",{
+    y: 50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller:"body",
+        start:"top 75%",
+        end:"top 70%",
+        scrub:3,
     }
 })
